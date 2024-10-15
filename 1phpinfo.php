750392c2de28f6037d2ab52f4e8b0844b5522a30
Ambe Mbong <ambenchang11@gmail.com>
2024-10-15 05:15:06 +0100

    first php file

---
+++ 1phpinfo.php
@@ -0,0 +1,14 @@
+<?php echo phpversion(); ?>
+<table border = "1">
+    <tr>
+        <th>Greeting</th>
+        <th>Name</th>
+    <tr>
+        <td>hello</td>
+        <td>ambe</td>
+    </tr>
+     <tr>
+        <td>hi</td>
+        <td>kee</td>
+    </tr>
+</table>
