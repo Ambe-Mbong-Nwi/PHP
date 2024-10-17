0bfb218d65e96e186a46618eeab12ccec2354e20
Ambe Mbong <ambenchang11@gmail.com>
2024-10-17 10:00:34 +0100

    form inserts into db

---
+++ 7SubmitCar.php
@@ -0,0 +1,96 @@
+<html>
+    <head>
+        <title>Car Inserted into Database</title>
+    </head>
+    <body bgcolor="#FFFFFF" text="#000000">
+        <?php 
+        //capturing the values entered by the user after submission
+            $VIN = $_POST['VIN'];
+            $Make  = $_POST['Make'];
+            $Model = $_POST['Model'];
+            $Price = $_POST['Asking_Price'];
+
+
+        //build insert query using the values above and assign to query variable
+        //use correct quotes or insert below might not work. back ticks are used for column names so here use single quotes.
+        $query = "INSERT INTO INVENTORY
+            (VIN, Make, Model, ASKING_PRICE)
+            VALUES (
+                '$VIN',
+                '$Make',
+                '$Model',
+                '$Price'
+                )";
+        
+        //print query to the browser
+        //to add two strings together in php use . eg below joing the query and the br.
+        echo($query."<br>");
+
+
+        //connect to the Cars database
+        //$mysqli is The object representing the MySQL connection
+        $mysqli = new mysqli('localhost', 'ambe', 'FocusMoi', 'Cars');
+
+        //check connection
+        if (mysqli_connect_error()) {
+            printf("connect failed: %s\n", mysqli_connect_error());
+            exit();
+        }
+        echo 'Connected successfully to mysql. <BR>';
+
+
+        //select database to work with
+        $mysqli->select_db("Cars");
+         echo("selected the cars database. <br>");
+
+         //inserting new car into database
+//          $result: A variable that will store the result of the query.
+//          $mysqli: The object representing the MySQL connection (created earlier).
+        // ->: This arrow operator is used in object-oriented programming in PHP. It accesses properties or methods of an object.
+        // query: A method of the $mysqli object that executes the SQL query.
+        // ($query): Passing the SQL query string as an argument to the query method.
+
+
+        //In PHP, the arrow operator (->) is used to access properties and methods of an object. In this case, $mysqli->query($query) calls the query method on the $mysqli object, which is an instance of the MySQLi class.
+         if($result = $mysqli->query($query)) {
+            echo "<p>You have successfully entered $Make $Model into the database</p>";
+         } else {
+            echo "Error entering $VIN into the database: ".mysqli_error($mysql)."<br>";
+         }
+
+         $mysqli->close();
+        ?>
+
+    </body>
+</html>
+
+
+
+
+
+<!-- difference between -> and => -->
+<!-- class Car {
+    public $make;
+    public function displayMake() {
+        return $this->make;
+    }
+}
+
+$myCar = new Car();
+$myCar->make = 'Honda'; // Using -> to set the property
+echo $myCar->displayMake(); // Using -> to call a method
+=> Operator
+Usage: This is the key-value pair operator used in arrays.
+Purpose: It is used to associate keys with values in associative arrays.
+Example:
+php
+Copy code
+$carDetails = [
+    'make' => 'Honda', // 'make' is the key, 'Honda' is the value
+    'model' => 'Civic'
+];
+
+echo $carDetails['make']; // Accessing the value using the key
+Summary
+->: Used for accessing methods and properties of an object (object-oriented).
+=>: Used to define key-value pairs in associative arrays. -->
---
+++ 7carForm.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>the joy of php</title>
+    </head>
+    <body>
+        <h1>Ambe's used cars</h1>
+        <!-- when submitted, pass form to php script in file below and use the post method to do that. -->
+        <form action="7SubmitCar.php" method="post">
+            VIN: <input name="VIN" type="text" /><br />
+            Make: <input name="Make" type="text"
+             /><br />
+            <br />
+            Model: <input name="Model" type="text"
+             /><br />
+            <br />
+            Price: <input name="Asking_Price"
+             type="text"
+             /><br />
+            <br />
+            <button name="Submit1"
+             type="submit"
+             value="submit"
+             >Submit </btuuon><br />
+        </form>
+    </body>
+</html>
